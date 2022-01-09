75e53b949c6918ebf50685f95105ff4f93f919da
Mauro Fillho <87574030+maurospbr@users.noreply.github.com>
2022-01-09 16:44:18 -0300

    Add files via upload

---
+++ checagemcaracteres.py
@@ -0,0 +1,13 @@
+print("Checagen de caracteres")
+nome = input("nome: ")
+print (len(nome))
+if len(nome)<4:
+   print("nome muito curto")
+if len(nome)<4:
+    print("entre novamente")
+elif len(nome) > 20:
+     print("nome muito grande")
+     print("utiliza no máximo 20 caracteres")
+     print("digite quantidade menor de caracteres")
+else:
+     print("nome  aceito")
